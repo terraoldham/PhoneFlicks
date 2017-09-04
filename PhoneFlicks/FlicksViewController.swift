017ba81891ac7d1a88833b00f3b65041a8a07942
Terra Oldham <toldham@hearsaycorp.com>
2017-09-04 07:42:16 -0700

    Table View

---
+++ PhoneFlicks/FlicksViewController.swift
@@ -0,0 +1,50 @@
+//
+//  FlicksViewController.swift
+//  PhoneFlicks
+//
+//  Created by Terra Oldham on 9/4/17.
+//  Copyright © 2017 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class FlicksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView .dequeueReusableCell(withIdentifier: "FlicksCell", for: indexPath)
+        cell.textLabel?.text = "row \(indexPath.row)"
+        print("row \(indexPath.row)")
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
